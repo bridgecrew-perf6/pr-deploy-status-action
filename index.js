25b39772060de0341de0586a1d1f2d4fceeba54d
GP <gp@envoy.com>
2022-01-21 14:27:19 -0800

    gp/use ref in filter

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const main = async() => {
     try {
         const repository = core.getInput('repository', {required: true});
         const sha = core.getInput('sha', {required: true});
+        const ref = core.getInput('ref', {required: true});
         const token = process.env.GITHUB_TOKEN;
         const octokit = new github.getOctokit(token);
 
         const { data: deployments } = await octokit.rest.repos.listDeployments({
             owner: repository.split('/')[0],
             repo: repository.split('/')[1],
+            ref
         });
 
         console.log(deployments);
