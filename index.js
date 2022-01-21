fa4fecbdba97ff9ab8cbbab9c42c7c44863feb46
GP <gp@envoy.com>
2022-01-21 14:32:10 -0800

    gp/use-pr-deployment-env

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const github = require('@actions/github');
 const main = async() => {
     try {
         const repository = core.getInput('repository', {required: true});
-        const sha = core.getInput('sha', {required: true});
-        const ref = core.getInput('ref', {required: true});
+        const environment = core.getInput('environment', {required: true});
         const token = process.env.GITHUB_TOKEN;
         const octokit = new github.getOctokit(token);
 
         const { data: deployments } = await octokit.rest.repos.listDeployments({
             owner: repository.split('/')[0],
             repo: repository.split('/')[1],
-            ref
+            environment,
         });
 
         console.log(deployments);
